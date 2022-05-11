9884951416a201dbdb218e9baf07e481f23b52e1
mtena <marco.tenas@gmail.com>
2022-05-11 14:49:01 -0500

    First Node js curl prototype

---
+++ CurlTest.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const querystring = require("querystring");
+//curl functionality 
+const { curly } = require('node-libcurl');
+
+
+//API_KEY test
+const API_KEY = process.env.API_KEY_WEATHER;
+console.log(API_KEY);
+
+//const { statusCode, data, headers } = await curly.get('http://www.google.com')
+myFunction(API_KEY,52776,8)
+console.log("notBroken");
+
+
+
+async function myFunction(API_KEY,ZIP,DAYS) {
+    // URL builder would be preferable 
+    const stringURL='http://api.weatherapi.com/v1/forecast.json?key='+API_KEY+'&q='+ZIP+'&days='+DAYS+'&aqi=no&alerts=no'
+    console.log(stringURL);
+    const json =JSON.stringify( await curly.get(stringURL));   
+    //const obj = JSON.parse(json);
+    const obj =JSON.parse(json);
+
+    for (var key in obj.data.forecast.forecastday) {
+        console.log(key)
+     }
+
+   // console.log(obj.data.forecast);
+
+  }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WeatherCodeTest
+Need to setup .env with API_KEY_WEATHER
